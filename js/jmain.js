f310aaa48763689295d688671c6b436e166cb062
Алла Королева <anet.sergeevna.89@mail.ru>
2025-03-25 17:38:03 +0300

    add index.html file

---
+++ js/jmain.js
@@ -0,0 +1,23 @@
+$(function(){
+    let tagP = $('#about p').text('I am a beginner web developer with a strong foundation in HTML, CSS, and JavaScript. I am passionate about creating visually appealing and user-friendly websites. Currently, I am in the process of expanding my skill set by learning the jQuery library, which will enhance my ability to create dynamic and interactive web applications. I am eager to continue my journey in web development, and I am excited to take on new challenges that will help me grow as a developer.Feel free to modify it to better fit your style or add any additional information!').hide(3000).show(2000);
+});
+
+$(function(){
+    $('.nav-list > li').hide(3000).show(2000);
+
+});
+
+$(function(){
+    let btn = $('.btn').width(200).height(50)
+    let form = $('.contact-input').width(500).height(50)
+    let text = $('.contact-input').color('red')
+});
+
+$(function(){
+    $('.nav-list li a').css('color', 'black').css('font-size', '25px')
+    $('.section-title').css('color', 'green').css('font-size', '40px')
+    $('.logo').css('color', 'black').css('font-size', '40px')
+    $('.header').css('background-color', 'gray')
+    
+});
+
